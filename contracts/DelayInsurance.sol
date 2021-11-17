7770f44edc5574a24df6d345f4263257b073fd45
Maiko Trindade <maikocharles@gmail.com>
2021-11-17 01:57:52 -0700

    Added complements to claim process and added more unit tests

--- contracts/DelayInsurance.sol
+++ contracts/DelayInsurance.sol
@@ -28,7 +28,7 @@ contract DelayInsurance is ChainlinkClient, KeeperCompatibleInterface {
     }
 
     struct Coverage {
-        address beneficiary;
+        address payable beneficiary;
         uint256 startDate;
         uint256 endDate;
         uint256 startPort;
@@ -54,13 +54,15 @@ contract DelayInsurance is ChainlinkClient, KeeperCompatibleInterface {
         uint256 policyId;
         Ship ship;
         Coverage coverage;
+        uint8 incidents;
     }
 
     event PolicySubscription(address indexed beneficiary, uint256 indexed id);
+    event IncidentReported(address indexed beneficiary, uint8 indexed actualNumberOfIncidents);
+    event PolicyPaidOut(address indexed beneficiary, uint256 indexed id);
 
     uint256 public lastTimeStamp;
 
-    //Policy[] public policies;
     mapping(address => Policy) public policies;
     Policy[] public upcomingPolicies;
 
@@ -72,6 +74,9 @@ contract DelayInsurance is ChainlinkClient, KeeperCompatibleInterface {
     address public trackingOracle;
     bytes32 public trackingJobId;
     uint256 public trackingFee;
+
+    // Threshold for triggering claiming process 
+    uint8 public incidentsThreshold = 1;
 
     // Prevents a function being run unless it's called by Insurance Provider
     modifier onlyOwner() {
@@ -116,7 +121,7 @@ contract DelayInsurance is ChainlinkClient, KeeperCompatibleInterface {
         );
 
         Coverage memory coverage = Coverage({
-            beneficiary: msg.sender,
+            beneficiary: payable(msg.sender),
             startDate: _startDate,
             endDate: _endDate,
             startPort: _startPort,
@@ -130,7 +135,8 @@ contract DelayInsurance is ChainlinkClient, KeeperCompatibleInterface {
         Policy memory policy = Policy({
             policyId: policyId,
             ship: ship,
-            coverage: coverage
+            coverage: coverage,
+            incidents: 0
         });
 
         policies[msg.sender] = policy;
@@ -264,20 +270,49 @@ contract DelayInsurance is ChainlinkClient, KeeperCompatibleInterface {
             policiesIndex < upcomingPolicies.length;
             policiesIndex++
         ) {
-            if (upcomingPolicies[policiesIndex].coverage.startDate >= block.timestamp && 
-                  upcomingPolicies[policiesIndex].coverage.endDate < block.timestamp) {
-      
-                policies[upcomingPolicies[policiesIndex].coverage.beneficiary].coverage.status = PolicyStatus.RUNNING;
+            Policy memory policy = upcomingPolicies[policiesIndex];
 
-                //TODO verify if any incidents occured (if insurance claim process can be trigger)
-                if (true) {
-                  // WIP
+            // verify valid policies 
+            if (upcomingPolicies[policiesIndex].coverage.startDate <= block.timestamp && 
+                  upcomingPolicies[policiesIndex].coverage.endDate > block.timestamp) {
+      
+                // Update policy status
+                policies[policy.coverage.beneficiary].coverage.status = PolicyStatus.RUNNING;
+
+                // TODO call external adapter which will verify if an incident occured based (location + weather)
+                if (hasIncident()) {
+                    // Update number of incidents
+                    policies[policy.coverage.beneficiary].incidents++;
+                    uint8 incidents = policies[policy.coverage.beneficiary].incidents;
+
+                    emit IncidentReported(policy.coverage.beneficiary, incidents);
+
+                    // Trigger claiming process using pre determined threshold
+                    if (incidents == incidentsThreshold) {
+                        payOut(policy.coverage.beneficiary, policy.coverage.premium, policy.policyId);
+                    }
                 }
 
-            } else if (upcomingPolicies[policiesIndex].coverage.endDate > block.timestamp) {
-              policies[upcomingPolicies[policiesIndex].coverage.beneficiary].coverage.status = PolicyStatus.COMPLETED;
-              delete upcomingPolicies[policiesIndex];
+            } else if (policy.coverage.endDate >= block.timestamp) {
+ 
+              // Update policy status
+              policies[policy.coverage.beneficiary].coverage.status = PolicyStatus.COMPLETED;
+              // remove current policy from upcomingPolicies list
+              delete policy;
             }
         }
+    }
+
+    // Verify incidents via External Adapter
+    function hasIncident(/* Input data */) public returns (bool) {
+        // TODO add a proper implementation
+        return true;
+    }
+
+    function payOut(address payable beneficiary, uint256 premium, uint256 policyId) public payable {
+        // transfer funds to beneficiary
+        (bool sent, bytes memory data) = beneficiary.call{value: premium}("");
+        require(sent, "Failed to transfer insurance claim");
+        emit PolicyPaidOut(beneficiary, policyId);
     }
 }
--- test/MainTest.js
+++ test/MainTest.js
@@ -1,11 +1,13 @@
-const { assert, expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const { ethers, waffle } = require("hardhat");
 
 describe("Delay Insurance test", function () {
 
   let DelayInsurance
   let delayInsurance
+  const provider = waffle.provider;
 
+  // Instantiate contract before each test
   beforeEach(async function () {
     DelayInsurance = await ethers.getContractFactory("DelayInsurance")
     delayInsurance = await DelayInsurance.deploy()
@@ -17,27 +19,56 @@ describe("Delay Insurance test", function () {
     let shipmentValue = 200000
     let pricePremium = shipmentValue / 200
 
+    // Trigger subscribePolicy method using mocked data
     let subscribePolicy = delayInsurance.connect(customer).subscribePolicy("shipId", shipmentValue, 1637386377, 1637559177, 1000, 2000, { from: customer.address, value: pricePremium })
 
     await expect(subscribePolicy).to.emit(delayInsurance, 'PolicySubscription').withArgs(customer.address, 0);
   });
 
+  it("should subscribe many policies", async function () {
+    const [admin, customer1, customer2, customer3] = await ethers.getSigners()
+    let shipmentValue = 200000
+    let pricePremium = shipmentValue / 200
+
+    // Trigger subscribePolicy method using mocked data and customer 1
+    let subscribePolicy1 = delayInsurance.connect(customer1).subscribePolicy("shipId1", shipmentValue, 1637386300, 1637559199, 1000, 2000, { from: customer1.address, value: pricePremium })
+    await expect(subscribePolicy1).to.emit(delayInsurance, 'PolicySubscription').withArgs(customer1.address, 0);
+
+    // Trigger subscribePolicy method using mocked data and customer 2
+    let subscribePolicy2 = delayInsurance.connect(customer2).subscribePolicy("shipId2", shipmentValue, 1637386322, 16375591755, 1000, 2000, { from: customer2.address, value: pricePremium })
+    await expect(subscribePolicy2).to.emit(delayInsurance, 'PolicySubscription').withArgs(customer2.address, 1);
+
+    // Trigger subscribePolicy method using mocked data and customer 3
+    let subscribePolicy3 = delayInsurance.connect(customer3).subscribePolicy("shipId3", shipmentValue, 1637386311, 1637559188, 1000, 2000, { from: customer3.address, value: pricePremium })
+    await expect(subscribePolicy3).to.emit(delayInsurance, 'PolicySubscription').withArgs(customer3.address, 2);
+  });
+
   it("should verify incidents", async function () {
-    const [admin, customer1, customer2] = await ethers.getSigners()
+    const [admin, customer1] = await ethers.getSigners()
     let shipmentValue = 500000
     let pricePremium = shipmentValue / 200
 
+    // Set very old startDate and a future endDate to make sure the vessel journey is currently happening
+    let startDate = 974448412; // startDate 17/11/2000
+    let endDate = 1763366812; // startDate 17/11/2025
+
+    // Get customer balance before insurance claim
+    const customerBalance = await provider.getBalance(customer1.address);
+    console.log("Customer balance: " + ethers.utils.formatEther(customerBalance))
+
+    // Trigger subscribePolicy method
     await delayInsurance
       .connect(customer1)
-      .subscribePolicy("shipId_customer1", shipmentValue, 1637386999, 1637576177, 1000, 2000, { from: customer1.address, value: pricePremium })
+      .subscribePolicy("shipId_customer1", shipmentValue, startDate, endDate, 1000, 2000, { from: customer1.address, value: pricePremium })
 
-    await delayInsurance
-      .connect(customer2)
-      .subscribePolicy("shipId_customer2", shipmentValue, 1637386377, 1637559177, 1000, 2000, { from: customer2.address, value: pricePremium })
-
+    // Trigger verifyIncidents method manually - this is being triggered by Chainlink Keepers
     await delayInsurance.connect(admin).verifyIncidents();
 
-    // Contract WIP
+    // Get customer balance after insurance claim
+    const customerBalance2 = await provider.getBalance(customer1.address);
+    console.log("Customer balance after verifyIncidents: " + ethers.utils.formatEther(customerBalance2))
+
+    // Test Incomplete - WIP 
   });
 
 });
