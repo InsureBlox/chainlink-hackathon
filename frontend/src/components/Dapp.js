035ea8cda88ad72cfa570374f7313815cd3fdcab
Malte Anthes <echo89@web.de>
2021-11-21 13:21:01 +0100

    Add unix date

--- frontend/src/components/Dapp.js
+++ frontend/src/components/Dapp.js
@@ -189,6 +189,14 @@ export class Dapp extends React.Component {
         </div>
 
         <div>State</div>
+        <div>
+          Unix departure date:{" "}
+          {(new Date(this.state.departureDate).getTime() / 1000).toString()}
+        </div>
+        <div>
+          Unix arrival date:{" "}
+          {(new Date(this.state.arrivalDate).getTime() / 1000).toString()}
+        </div>
         <div className="word-break">{JSON.stringify(this.state)}</div>
 
         <div className="row">
