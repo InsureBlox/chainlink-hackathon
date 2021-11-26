e8f53077cb960a69300fc252a103b93e9b40ddf0
cNoveron <carlos.escom.ipn@gmail.com>
2021-11-26 11:23:37 -0600

    visual improvement

--- frontend/src/components/Dapp.js
+++ frontend/src/components/Dapp.js
@@ -10,6 +10,8 @@ import { NoWalletDetected } from "./NoWalletDetected";
 import { ConnectWallet } from "./ConnectWallet";
 import { TransactionErrorMessage } from "./TransactionErrorMessage";
 
+import background from "../assets/sea.jpg"
+
 import "../index.css";
 
 import ports from "../api/ports.json";
@@ -83,14 +85,75 @@ export class Dapp extends React.Component {
     }
 
     return (
+      <>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+          <div class="container">
+            <a class="navbar-brand" href="#">
+              <b>⛈️ Ocean Storm</b> <h6 display="inline">by InsureBlox</h6>
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            {/* <div> */}
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                      Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                  </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+              </div>
+
+            {/* </div> */}
+
+          </div>
+
+        </nav>
+
+        <div className="jumbotron" style={{padding: '0', height: '6em', overflow: 'hidden'}}>
+          <img className="img-fluid" src={background} alt="" style={{width: '100%'}}/>
+        </div>
+
       <div className="container p-4">
         <div className="row">
           <div className="col-12">
-            <h1>⛈️ Ocean Storm by InsureBlox</h1>
+              <h2>🚢 <b>Shipment Delay Policy</b></h2>
           </div>
         </div>
 
         <hr />
+
+        <div className="row">
+          <div className="col-12">
+            <p>Subscribe to a shipment delay policy today!</p>
+          </div>
+        </div>
+
 
         <div className="row">
           <div className="col-12">
@@ -502,6 +565,7 @@ export class Dapp extends React.Component {
           </button>
         </div>
       </div>
+    </>
     );
   }
 
