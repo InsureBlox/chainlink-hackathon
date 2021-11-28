490d433bfd37c9cc0f83886da5bb465d1f892411
cNoveron <carlos.escom.ipn@gmail.com>
2021-11-28 12:06:42 -0600

    remove search bar

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -40,20 +40,6 @@ export function Navigation() {
                 </Link>
               </li>
             </ul>
-            <form className="form-inline my-2 my-lg-0 d-none d-md-block">
-              <input
-                className="form-control mr-sm-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button
-                className="btn btn-outline-success my-2 my-sm-0"
-                type="submit"
-              >
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
